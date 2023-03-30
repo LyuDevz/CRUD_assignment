e67dcd9355ddd65704d27c5b425aa78a9f3cba75
LyuDevz <lyuii0726@gmail.com>
2023-03-30 21:04:11 +0900

    final

--- session/blog/views.py
+++ session/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Blog
+from .forms import BlogForm
 
 def home(request):
     blogs = Blog.objects.all()
@@ -12,24 +13,47 @@ def detail(request,blog_id):
 def new(request):
     return render(request,'new.html')
     
+# def create(request):
+#     new_blog = Blog()
+#     new_blog.title = request.POST['title']
+#     new_blog.content = request.POST['content']
+#     new_blog.save()
+#     #return render(request, 'detail.html', {'blog':new_blog})
+#     return redirect('detail',new_blog.id)
+
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.content = request.POST['content']
-    new_blog.save()
-    return redirect('detail',new_blog.id)
+    form = BlogForm(request.POST)
+    # 착한 사용자
+    if form.is_valid():
+        new_blog = form.save(commit = False)
+
+        new_blog.save()
+        return redirect('detail', new_blog.id)
+    # 나쁜 사용자
+    return render(request, 'new.html')
 
 def edit(request, blog_id):
     edit_blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'edit.html', {'edit_blog': edit_blog})
 
+# def update(request, blog_id):
+#     old_blog = get_object_or_404(Blog, pk=blog_id)
+#     old_blog.title = request.POST['title']
+#     old_blog.content = request.POST['content']
+#     old_blog.save()
+#     return redirect('detail', old_blog.id)
+
 def update(request, blog_id):
     old_blog = get_object_or_404(Blog, pk=blog_id)
-    old_blog.title = request.POST['title']
-    old_blog.content = request.POST['content']
-    old_blog.save()
-    return redirect('detail', old_blog.id)
-
+    form = BlogForm(request.POST, instance=old_blog)
+    
+    #착한 사용자
+    if form.is_valid():
+        new_blog = form.save(commit = False)
+        new_blog.save()
+        return redirect('detail', old_blog.id)
+    # 나쁜 사용자
+    return render(request, 'new.html') 
 
 def delete(request, blog_id):
     delete_blog = get_object_or_404(Blog, pk=blog_id)
